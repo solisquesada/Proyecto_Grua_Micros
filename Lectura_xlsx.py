dd71f29e9ab1f66d18110fb17f411d4a672f6abf
solisquesada <141278516+solisquesada@users.noreply.github.com>
2023-08-06 23:24:26 -0600

    Lectura_xlsx.py
    Se añaden los códigos de error por lectura 10# y se corrigen pulgas

--- Lectura_xlsx.py
+++ Lectura_xlsx.py
@@ -6,22 +6,30 @@ import openpyxl
 # la matriz que representa el patrón y otra con la matríz que
 # representa el suministro.
 def leer_archivo(archivo):
+    Error = 0 # Se empieza el error en 0
+    # Se definen las matrizes a llenar
+    matrizPatron = [[None for _ in range(5)] for _ in range(5)]
+    matrizSuministro = [[None for _ in range(5)] for _ in range(5)]
+    matrizCarga = [[None for _ in range(5)] for _ in range(5)]
+
     try:
         # Se abre el archivo .xlsx
         workbook = openpyxl.load_workbook(archivo)
         sheet = workbook.active
     except FileNotFoundError:
-        # En caso de no encontrarse el archivo se avisa al usuario 
+        # En caso de no encontrarse el archivo se avisa al usuario
+        Error = 101 # Se define el error 101 como error por no archivo
         print(f"El archivo '{archivo}' no fue encontrado.")
-        return
+        return (matrizPatron, matrizSuministro, matrizCarga, Error)
+    except PermissionError:
+        # En caso de no tener permisos al archivo avisa al usuario
+        Error = 102 # Se define el error 102 como error por archivo bloqueado
+        print(f"El archivo '{archivo}' está bloqueado.")
+        return (matrizPatron, matrizSuministro, matrizCarga, Error)
 
-    # Se definen las matrizes a llenar
-    matrizPatron = [[None for _ in range(5)] for _ in range(5)]
-    matrizSuministro = [[None for _ in range(5)] for _ in range(5)]
-
-    # Se estudia el patron dado por el usuario
-    for fila in range(1, 5):
-        for columna in range(1, 5):
+    # Se estudia el estado del patron dado por el usuario
+    for fila in range(1, 6):
+        for columna in range(1, 6):
             celda = sheet.cell(row=fila, column=columna).value
             if celda == "A":
                 matrizPatron[fila-1][columna-1] = "A"
@@ -32,8 +40,8 @@ def leer_archivo(archivo):
             else:
                 matrizPatron[fila-1][columna-1] = None
 
-    # Se estudia la matriz formada por el suministro
-    for fila in range(1, 5):
+    # Se estudia el estado de la matriz formada por el suministro
+    for fila in range(1, 6):
         for columna in range(6, 11):
             celda = sheet.cell(row=fila, column=columna).value
             if celda == "A":
@@ -43,19 +51,34 @@ def leer_archivo(archivo):
             elif celda == "C":
                 matrizSuministro[fila-1][columna-6] = "C"
             else:
-                matrizPatron[fila-1][columna-6] = None            
+                matrizSuministro[fila-1][columna-6] = None            
+
+    # Se estudia el estado de la matriz de carga
+    for fila in range(1, 6):
+        for columna in range(11, 16):
+            celda = sheet.cell(row=fila, column=columna).value
+            if celda == "A":
+                matrizCarga[fila-1][columna-11] = "A"
+            elif celda == "B":
+                matrizCarga[fila-1][columna-11] = "B"
+            elif celda == "C":
+                matrizCarga[fila-1][columna-11] = "C"
+            else:
+                matrizCarga[fila-1][columna-11] = None  
 
     # Para que esta función funcione, el archivo tiene que tener el formato dado
     # por nosotros en la plantilla de excel.
-    return matrizPatron, matrizSuministro
+    return (matrizPatron, matrizSuministro, matrizCarga, Error)
 
 def imprimir_matriz(matriz):
+    print ("\nIMPRIMIENNDO MATRIZ:\n")
     for fila in matriz:
         print(fila)
 
 if __name__ == "__main__":
     archivo = "archivo.xlsx"  # Reemplaza con la ruta a tu archivo XLSX o CSV
-    matrizPatron, matrizSuministro = leer_archivo(archivo)
+    matrizPatron, matrizSuministro, matrizCarga, Error = leer_archivo(archivo)
     imprimir_matriz(matrizPatron)
     imprimir_matriz(matrizSuministro)
+    imprimir_matriz(matrizCarga)
 
